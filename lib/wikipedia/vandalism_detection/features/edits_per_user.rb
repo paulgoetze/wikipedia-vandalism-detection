888fb72648bd162477619853fe46da52e4d31abf
Paul Goetze <paul.christoph.goetze@gmail.com>
2014-05-05 19:35:37 +0200

    Fix edits per user feature

--- lib/wikipedia/vandalism_detection/features/edits_per_user.rb
+++ lib/wikipedia/vandalism_detection/features/edits_per_user.rb
@@ -9,6 +9,8 @@ module Wikipedia
       # This feature calculates the number of submitted edits by the same editor (IP or ID) as the edit's editor.
       class EditsPerUser < Base
 
+        # Returns the number of edits the edit's editor made in the same article.
+        # Attention! This is pretty time consuming (~2sec) due to the url request.
         def calculate(edit)
           super
 
@@ -17,9 +19,10 @@ module Wikipedia
           url = "http://en.wikipedia.org/w/api.php?action=query&format=xml&list=usercontribs&ucuser=#{user}&ucprop=ids"
 
           content = URI.parse(url).read
-          puts "content: #{content}"
-          items = Nokogiri::XML(content).to_a.to_s
 
+          xml = Nokogiri::XML(content)
+          page_id =  xml.xpath("//item[@revid='#{revision.id}']").first.xpath("@pageid").text
+          xml.xpath("//item[@pageid='#{page_id}']").count
         end
       end
     end
