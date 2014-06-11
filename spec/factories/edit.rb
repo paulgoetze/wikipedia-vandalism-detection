e952911ffff9c9c136fec4f9606708d94904c86e
Paul Goetze <paul.christoph.goetze@gmail.com>
2014-06-11 14:25:07 +0200

    Add page_id to edit factory

--- spec/factories/edit.rb
+++ spec/factories/edit.rb
@@ -3,14 +3,16 @@ FactoryGirl.define do
   factory :edit, class: Wikipedia::VandalismDetection::Edit do
     old_revision { FactoryGirl.build(:old_revision) }
     new_revision { FactoryGirl.build(:new_revision) }
+    page_id nil
 
-    initialize_with { new(old_revision, new_revision) }
+    initialize_with { new(old_revision, new_revision, page_id) }
   end
 
   factory :anonymous_edit, class: Wikipedia::VandalismDetection::Edit do
     old_revision { FactoryGirl.build(:old_revision) }
     new_revision { FactoryGirl.build(:anonymous_revision) }
+    page_id = nil
 
-    initialize_with { new(old_revision, new_revision) }
+    initialize_with { new(old_revision, new_revision, page_id) }
   end
 end
