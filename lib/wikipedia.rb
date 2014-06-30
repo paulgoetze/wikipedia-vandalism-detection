0e0190014a0619df2b2477454669007dd3239d5a
Paul Goetze <paul.christoph.goetze@gmail.com>
2014-06-30 13:48:28 +0200

    Add request retry for API calls in case of timeout

--- lib/wikipedia.rb
+++ lib/wikipedia.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'timeout'
 
 module Wikipedia
 
@@ -15,15 +16,35 @@ module Wikipedia
     params.map{ |k, v| "#{k}=#{v}" }.join('&')
   end
 
+  # Retries to call the request in the case of Timeout errors
+  def self.request_with_retry(uri, times = 1, timeout = 5)
+    content = ""
+
+    begin
+      Timeout::timeout(timeout) do
+        content = URI.parse(uri).read
+      end
+    rescue Timeout::Error
+      if times > 0
+        times -= 1
+        retry
+      else
+        raise Timeout::Error,  "#{times} times retrying request failed."
+      end
+    end
+
+    content
+  end
+
   def api_request(params = {})
     uri = URI::encode(api_base_uri + param_string(params))
-    content = URI.parse(uri).read
+    content = request_with_retry(uri, 3)
     Nokogiri::XML(content)
   end
 
   def wikitrust_request(params = {})
     uri = URI::encode(wikitrust_base_uri + param_string(params))
-    URI.parse(uri).read
+    request_with_retry(uri, 3)
   end
 
   module_function :api_request, :wikitrust_request
